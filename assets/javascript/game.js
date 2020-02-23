9bfdfeedffd2b2e83f20d14fc23986a87f654dae
Laura Faye <jim.laura2@tds.net>
2020-02-23 14:52:29 -0500

    Modified all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,18 @@
 // Execute this code when the DOM has fully loaded.
 $(document).ready(function() {
   
-  
+   var randomNum = randomNumGen();
+
     var wins = 0;
     var losses = 0;
     var crystals;
-    
+
+    $(".btn").on("click", function() {
+        console.log("I've been clicked!");
+      });
+
+})
+
+function randomNumGen() {
+  return Math.floor(Math.random() * 102) + 19;
+}
